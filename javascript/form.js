b423aeec4327beb07d0f05d8da74d14b02451479
Transformer1997 <jose.manuel.oberreuter@gmail.com>
2023-01-04 21:56:45 -0300

    contact chages

---
+++ javascript/form.js
@@ -0,0 +1,136 @@
+const form = document.getElementById("contactForm");
+const divDisplay = document.getElementById("afterSend");
+
+// SENDING FORM
+form.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    form.style.display = "none";
+    divDisplay.style.display = "block";
+
+    console.log('Formulario enviado')
+    
+});
+
+// CHECK INPUTS 
+function checkName(){
+    const name = document.getElementById("inputname");
+    const nameValue = name.value;
+    const nameTrim = nameValue.trim()
+
+    const errorName = document.getElementById('errorName');
+
+    if (nameTrim === '' || nameValue == null){
+        name.style.border = '3px solid red';
+        errorName.style.display = 'block';
+        errorName.innerHTML = '<b>Debes escribir un nombre </b>';
+    } else {
+        name.style.border = '3px solid green';
+        errorName.style.display = 'none'
+    }
+}
+
+
+function checkEmail(){
+    const email = document.getElementById("inputemail");
+    const emailValue = email.value;
+    const emailTrim = emailValue.trim();
+
+    const errorEmail = document.getElementById('errorEmail');
+
+    if (emailTrim === '' || emailValue == null){
+        email.style.border = '3px solid red'
+        errorEmail.style.display = 'block';
+        errorEmail.innerHTML = '<b>Debes escribir un correo electronico</b>'
+    } else {
+        email.style.border = '3px solid green';
+        errorEmail.style.display = 'none'
+    }
+}
+
+function checkEmailConf(){
+
+    const email = document.getElementById("inputemail");
+    const emailValue = email.value;
+
+    const emailconf = document.getElementById("emailconf");
+    const emailconfValue = emailconf.value;
+    const emailconfTrim = emailconfValue.trim();
+
+    const errorEmailConf = document.getElementById('errorEmailConf');
+    const errorEmail = document.getElementById('errorEmail');
+
+
+    if (emailconfTrim === '' || emailconfValue == null){
+        emailconf.style.border = '3px solid red'
+        errorEmailConf.style.display = 'block';
+        errorEmailConf.innerHTML = '<b>Debes escribir nuevamente el correo electronico</b>'
+    
+    } else {
+
+        // Comparar si ambos valores son iguales
+        if (emailValue !== emailconfValue) {
+            // Mostrar mensaje de error
+            errorEmail.innerHTML = "<b>Los correos no coinciden</b>";
+            errorEmailConf.innerHTML = "<b>Los correos no coinciden</b>";
+        }     
+        
+        emailconf.style.border = '3px solid green';
+        errorEmailConf.style.display = 'none'
+    }
+
+}
+
+
+function checkPhone() {
+    const phone = document.getElementById("phone");
+    const phoneValue = phone.value;
+    const phoneTrim = phoneValue.trim();
+    const errorPhone = document.getElementById('errorPhone');
+
+    if (phoneTrim === '' || phoneValue == null){
+        phone.style.border = '3px solid red'
+        errorPhone.style.display = 'block';
+        errorPhone.innerHTML = '<b>Debes escribir un numero de telefono</b>'
+    } else {
+        phone.style.border = '3px solid green';
+        errorPhone.style.display = 'none'
+    }
+}
+
+
+function checkUbi() {
+    const ubi = document.getElementById("ubicacion");
+    const ubiValue = ubi.value;
+    const ubiTrim = ubiValue.trim();
+    const errorUbi = document.getElementById('errorUbi');
+
+    if (ubiTrim === '' || ubiValue == null){
+        ubi.style.border = '3px solid red';
+        errorUbi.style.display = 'block';
+        errorUbi.innerHTML = '<b>Debes escribir una dirección</b>';
+    } else {
+        ubi.style.border = '3px solid green';
+        errorUbi.style.display = 'none';
+    }
+}
+
+
+ 
+
+function checkMessage(){
+    const message = document.getElementById("message");
+    const messageValue = message.value;
+    const messageTrim = messageValue.trim();
+    const errormessage = document.getElementById('errorMessage');
+
+    if (messageTrim === '' || messageValue == null){
+        message.style.border = '3px solid red'
+        errormessage.style.display = 'block';
+        errormessage.innerHTML = '<b>Debes escribir un mensaje</b>'
+    } else {
+        message.style.border = '3px solid green';
+        errormessage.style.display = 'none'
+    }
+}
